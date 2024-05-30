f23a305f4dffb85df7c35200e27eb9bd01d8b027
vikashkumar6207 <vktd828403@gmail.com>
2024-05-30 12:04:19 +0530

    complete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('', (req, res) => {
+app.get('/user/:userId', (req, res, next) => {
   //Write a code here for endpoint /user/:userId and also print parameter in json form
+  
+  const userId = req.params.userId;
+  if(isNaN(userId)){
+    const error = new Error("UserId must be a number");
+    error.status = 400;
+    return next(error);
+  }
+  res.status(200).json({userId});
 });
 
 module.exports = app;
